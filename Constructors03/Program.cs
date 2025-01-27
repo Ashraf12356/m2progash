91291f8bf591c98506271e299a721390b9ee4598
Ashraf12356 <ashraffouhaddou@outlook.com>
2025-01-27 11:41:33 +0100

    first commit

---
+++ Constructors03/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Constructors03
+{
+    internal class Program
+    {
+        private void.Run()
+        { { QuizVraag quizVraag = new QuizVraag("hoe springt een konijn", "Met zijn poten"); } }
+        class QuizVraag
+        {
+            internal string vraag;
+            internal string antwoord;
+
+            internal QuizVraag(string vraag, string antwoord)
+            { this.vraag = vraag;
+                this.antwoord = antwoord;
+            }
+        }
+        Program program = new Program();
+        }
+    }
+
